82e4348ae8eff6def76b05cf6f637603004b1a56
Professional-Miles <mileslowe@pm.me>
2021-10-23 21:15:49 +0100

    Refactored the main app into an app package.

--- src/main/java/com/sparta/ml/northwindrest/app/NorthwindRestApplication.java
+++ src/main/java/com/sparta/ml/northwindrest/app/NorthwindRestApplication.java
@@ -1,4 +1,4 @@
-package com.sparta.ml.northwindrest;
+package com.sparta.ml.northwindrest.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
